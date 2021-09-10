ba0b29ebe9e90cd5ec8f8b30b70b7d4be0f43b25
Rawaha M <rawaha.m@gmail.com>
2021-09-10 10:58:18 -0700

    get session user route test successful

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -39,6 +39,19 @@ router.delete(
 }
 );
 
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
+
 
 
 
