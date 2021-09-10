b90934bf04db8881ee726d3d83bbf29e2b42d39c
Rawaha M <rawaha.m@gmail.com>
2021-09-10 14:42:22 -0700

    css test

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import './LoginForm.css';
 
 function LoginFormPage() {
   const dispatch = useDispatch();
@@ -47,7 +48,7 @@ function LoginFormPage() {
           required
         />
       </label>
-      <button type="submit">Log In</button>
+      <button className='login-btn' type="submit">Log In</button>
     </form>
   );
 }
