41db73b2174c856700554c85b76f579a67407446
Rawaha M <rawaha.m@gmail.com>
2021-09-10 10:11:49 -0700

    finihsed testing auth successfully

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
 
 
 router.post('/test', function(req, res) {
@@ -18,6 +20,24 @@ router.get('/set-token-cookie', asyncHandler(async (req, res) => {
   setTokenCookie(res, user);
   return res.json({ user });
 }));
+
+
+router.get(
+  '/restore-user',
+  restoreUser,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 
 
