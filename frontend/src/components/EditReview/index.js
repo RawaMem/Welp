70ce18d5c44396db443f76f89277baa9ead697dd
Rawaha M <rawaha.m@gmail.com>
2021-09-19 23:10:57 -0700

    name in reviews

--- frontend/src/components/EditReview/index.js
+++ frontend/src/components/EditReview/index.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
+import { listOfAllBusinesses } from '../../store/businesses';
 import { editReviewDetails } from '../../store/reviews';
 import { Footer } from '../Footer';
 import './EditReview.css'
@@ -19,6 +20,19 @@ export const EditReview = () => {
 
     const [rating, setRating] = useState('');
     const [content, setContent] = useState('');
+
+    const AllBusiness = useSelector(state => {
+        return state.businesses.list
+    });
+
+    const currentBusiness = AllBusiness.find(business => {
+        return +businessId === business.id
+    })
+
+    useEffect(() => {
+        dispatch(listOfAllBusinesses())
+        // dispatch(allReviewsForBusiness())
+    }, [dispatch]);
 
 
     const updateRating = (e) => setRating(e.target.value);
@@ -43,6 +57,7 @@ export const EditReview = () => {
 
       return (
         <section className="form">
+            <h2 className="review-business-title">{currentBusiness?.title}</h2>
             <form onSubmit={handleSubmit}>
                 <div className="r-box-form">
                     <input
